18d1f3abaf0d1d87dd632b4f43f4e99e707ecad0
Alexa <20253ds109@utez.edu.mx>
2026-01-18 16:41:00 -0600

    Subiendo archivos del proyecto

---
+++ MergeWithOutConflict/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void Main(String[] args){
+        System.out.println("Semana2 - Version base");
+        System.out.println("2 + 2 = 4 - 5 * 3 = 15");
+    }
+}
