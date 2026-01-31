d595e920be3499badeec39af7f5e2b77d7034416
Alexa <20253ds109@utez.edu.mx>
2026-01-30 21:21:41 -0600

    se hace trabajo

--- CALCU2/src/Main.java
+++ CALCU2/src/Main.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int opcion;
         int total = 0;
+
         int cAF = 0;
         int fAC = 0;
         int kmAM = 0;
         int mAKm = 0;
 
         do {
-            System.out.println("SELECCIONE UNA OPCIÓN:");
-            System.out.println("1) °C a °F");
-            System.out.println("2) °F a °C");
-            System.out.println("3) Km a Millas");
-            System.out.println("4) Millas a Km");
-            System.out.println("5) Salir");
+            System.out.println("--------------");
+            System.out.println("1- C° a F°");
+            System.out.println("2- F° a C°");
+            System.out.println("3- Km a Millas");
+            System.out.println("4- Millas a Km");
+            System.out.println("5- Salir");
             System.out.print("Elige una opcion: ");
 
-            // Validar opción
             if (!sc.hasNextInt()) {
-                System.out.println("Opcion invalida, debe ser un numero.");
+                System.out.println("Debe ser un numero.");
                 sc.next();
                 continue;
             }
@@ -51,13 +51,13 @@ public class Main{
             switch (opcion) {
                 case 1:
                     resultado = (valor * 9 / 5) + 32;
-                    System.out.println(valor + " °C = " + resultado + " °F");
+                    System.out.println(valor + "° C = " + resultado + "° F");
                     cAF++;
                     break;
 
                 case 2:
                     resultado = (valor - 32) * 5 / 9;
-                    System.out.println(valor + " °F = " + resultado + " °C");
+                    System.out.println(valor + "° F = " + resultado + "° C");
                     fAC++;
                     break;
 
@@ -79,11 +79,10 @@ public class Main{
         } while (true);
 
         System.out.println("Total de conversiones: " + total);
-        System.out.println("°C a °F: " + cAF);
-        System.out.println("°F a °C: " + fAC);
+        System.out.println("C° a F°: " + cAF);
+        System.out.println("F° a C°: " + fAC);
         System.out.println("Km a Millas: " + kmAM);
         System.out.println("Millas a Km: " + mAKm);
 
-        System.out.println("Programa finalizado.");
     }
 }
