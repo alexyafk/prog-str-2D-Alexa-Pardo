14750c45befe344f0499e13dfd029e138a7f219d
alexyafk <20253ds109@utez.edu.mx>
2026-02-06 09:26:48 -0600

    tarea

---
+++ CotizadorDeEnvio/src/ShippingCalculator.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ShippingCalculator {
+   public double pesokg;
+   public int distanciakm;
+   public int tipoServicio;
+   public boolean esZonaRemota;
+
+   public double calcularSubTotal(double pesokg,int distanciakm, )
+}
